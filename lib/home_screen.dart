efb461e7515bce3f78a9155563ce57505b3a6219
arakawan <arakawan@pivot.jp>
2020-11-15 02:29:02 +0900

    アプリ完成

---
+++ lib/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  String _textHeader = "あなたの名前を教えてください";
+  TextEditingController _textEditingController = TextEditingController();
+  Widget _imageWidget = Container();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("はじめてのアプリ!!"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Center(
+                child: Text( _textHeader, style: TextStyle(fontSize: 10.0))),
+            TextField(
+              keyboardType: TextInputType.text,
+              style: TextStyle(fontSize: 20.0),
+              controller: _textEditingController, //TODO 入力した文字を管理するTextEditingControllerの設定
+            ),
+            Expanded(child: _imageWidget),
+            SizedBox(
+              width: double.infinity,
+              child: RaisedButton(
+                onPressed: () => _onClick(), //ボタンのクリック処理
+                child: Text("おしてね！！！", style: TextStyle(fontSize: 18.0),),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(),
+    );
+  }
+
+  _onClick() {
+    setState(() {
+      // 文字
+      var inputText = _textEditingController.text;
+      _textHeader = "こんにちは！$inputTextさん";
+
+      // 画像
+      _imageWidget = Image.asset("assets/images/hello.png");
+    });
+  }
+}
